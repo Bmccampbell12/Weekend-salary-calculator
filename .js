93dc0b83ed0e57434fe194a8e53ea05ac98e5c7b
Bruce McCampbell <mccampbellbruce1@gmail.com>
2024-09-07 20:22:40 -0500

    second commit made: updated .js file with table body function/ submit text framework, 5 essential fields, submit and clear field buttons added, table ID added, style.css updated with borrowed framework from affirmations project

--- .js
+++ .js
@@ -3,3 +3,29 @@ onReady();
 function onReady() {
   console.log('Javascript is working!');
 }
+
+let tableBody = document.getElementById("table")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function submitText(){
+  let deleteButton = `<td onClick="deleteAffirmation(event)">❌</td>`
+  let formAffirmation = document.getElementById("affirmation").value  
+  let formAuthor = document.getElementById("Author").value   
+  tableBody.innerHTML +=   `<tr><td>${formAffirmation}</td><td>${formAuthor}</td>${deleteButton}</tr>`
+  event.preventDefault();
+} 
