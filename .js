19f9c9f9c3804a85a1ad02032aff017e9582be4b
Bruce McCampbell <mccampbellbruce1@gmail.com>
2024-09-07 23:01:32 -0500

    update madeto .js file and index.html. table field populated and .css file correctly referenced

--- .js
+++ .js
@@ -16,25 +16,24 @@ let tableBody = document.getElementById("table")
 
 
 
-function clearFields(event){
-  let parentRow = event.target.parentElement
+function clearFields(event) {
+  let parentRow = event.target.parentElement   // deleting function: will clear fields 
   parentRow.remove()
 }
 
 
-
-function submitText(){
-  let clearFields = `<button onClick="clearFields(event)">Clear Fields</button>`
+function submitText() {
+  let clearFields = `<button onClick="clearFields(event)">Clear Fields</button>` //creates the event that will clear the input fields
 
   let firstNameInput = document.getElementById("firstNameInput").value;
   let lastNameInput = document.getElementById("lastNameInput").value;
   let idInput = document.getElementById("firstNameInput").value;
   let titleInput = document.getElementById("firstNameInput").value;
   let annualSalaryInput = document.getElementById("firstNameInput").value;
- 
 
 
 
-  tableBody.innerHTML += `<tr><td>${firstNameInput}</td><td>${lastNameInput}</td><td>${idInput}</td><td>${titleInput}</td><td>${annualSalaryInput}</td>${clearFields}</tr>`
-  (event).preventDefault();
+
+  tableBody.innerHTML += `<tr><td>${firstNameInput}</td><td>${lastNameInput}</td><td>${idInput}</td><td>${titleInput}</td><td>${annualSalaryInput}</td></tr>`
+    event.preventDefault();
 } 
